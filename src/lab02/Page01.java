d34adc2daa3f36e9c77aec343710290edbfc0913
Lei Zhao <Lei Zhao@10.38.24.217>
2016-08-31 11:21:56 -0400

    initial commit

---
+++ src/lab02/Page01.java
@@ -0,0 +1,17 @@
+package lab02;
+
+public class Page01
+{
+
+	public static void main (String[] args)
+	
+	{
+	String  s = "";
+	s = s + "A";
+	s = s + "C";
+	s = s + "GT";
+	
+	System.out.println(s);
+	}
+
+}
