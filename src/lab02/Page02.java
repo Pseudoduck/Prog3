51d36efd29d808e2021174dd756b324248f9b93a
Lei Zhao <Lei Zhao@10.38.24.217>
2016-08-31 11:35:44 -0400

    initial commit

---
+++ src/lab02/Page02.java
@@ -0,0 +1,17 @@
+package lab02;
+
+import java.util.Random;
+
+public class Page02
+{
+
+	public static void main (String[] args)
+	
+	{
+		Random random = new Random();
+		System.out.println(random.nextInt(4));
+		System.out.println(random.nextInt(4));
+	}
+	
+}
+
