0de3358b86ef665b4c39fb6dce01acb69959487e
Lei Zhao <Lei Zhao@lzhao13>
2016-08-30 22:12:44 -0400

    initial commit

---
+++ src/lab01/Sample1.java
@@ -0,0 +1,17 @@
+package lab01;
+
+public class Sample1
+
+ {
+
+     public static void main (String[] args)
+
+     {
+         int x = 4;
+
+         if (x == 4)
+
+             System.out.println ("Yes");
+     }
+
+ }
