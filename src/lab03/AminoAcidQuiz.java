5988858388135d0e919c46d8d2ddf91d2d3bc79b
lzhao13 <lzhao13@ccibio226-011.dyn.uncc.edu>
2016-09-07 12:04:06 -0400

    INITIAL COMMIT

---
+++ src/lab03/AminoAcidQuiz.java
@@ -0,0 +1,30 @@
+package lab03;
+
+import java.util.Random;
+
+public class AminoAcidQuiz 
+
+{
+	
+	public static String[] SHORT_NAMES = 
+		{ "A","R", "N", "D", "C", "Q", "E", 
+		"G",  "H", "I", "L", "K", "M", "F", 
+		"P", "S", "T", "W", "Y", "V" };
+	
+	public static String[] FULL_NAMES = 
+		{"alanine","arginine", "asparagine", 
+		"aspartic acid", "cysteine",
+		"glutamine",  "glutamic acid",
+		"glycine" ,"histidine","isoleucine",
+		"leucine",  "lysine", "methionine", 
+		"phenylalanine", "proline", 
+		"serine","threonine","tryptophan", 
+		"tyrosine", "valine"};
+
+	public static void main(String[] args)
+	{
+		Random r = new Random();
+		int randomIndex = r.nextInt(FULL_NAMES.length);
+		System.out.println(FULL_NAMES[randomIndex]);
+	}
+}
