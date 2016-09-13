51863cdfcb8115f51646c8132814289f8aa04267
Lei Zhao <Lei Zhao@10.38.4.47>
2016-09-13 16:48:15 -0400

    grading commit [1]

--- src/lab03/AminoAcidsQuiz.java
+++ src/lab03/AminoAcidsQuiz.java
@@ -20,55 +20,82 @@ public class AminoAcidsQuiz
 		"serine","threonine","tryptophan", 
 		"tyrosine", "valine"};
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
 		boolean b = true;
-		int score = 0;
+		int score = 0;		
+		float secondsLimit = 30f;
+		float millisToSeconds = 1000f;
+		String quit = "quit";
 		long startTime = System.currentTimeMillis();
-		long secondsLimit = 30;
-		long millisToSeconds = 1000;
+		
 		do
 		{
 			Random r = new Random();
 			int randomIndex = r.nextInt(FULL_NAMES.length);
 			System.out.println(FULL_NAMES[randomIndex]);
-			System.out.println("Plase enter one letter character for this amino acid:");					
+			System.out.println("Enter the one letter character for this amino acid (or type 'quit' to quit the quiz):");					
 			Scanner scan = new Scanner(System.in);
 			String s = scan.next();
-			String aChar = "" + s.charAt(0);
-			System.out.println(aChar.toUpperCase());
 			
-			if(aChar.toUpperCase().equals(SHORT_NAMES[randomIndex]))
+			if(s.equals(quit)) //detect if user decides to quit
 			{
-				long endTime = System.currentTimeMillis();
-				long diffTime = endTime - startTime;
-				if (diffTime/1000 < 30)
-					{
+				break;
+			}
+			
+			else 
+			{
+				String aChar = "" + s.charAt(0);
+				System.out.println(aChar.toUpperCase());
+			
+				if(aChar.toUpperCase().equals(SHORT_NAMES[randomIndex])) // check if the input is correct
+				{
+					long endTime = System.currentTimeMillis();
+					long diffTime = endTime - startTime;
+					
+					if (diffTime/millisToSeconds < secondsLimit) // check if the test running is time less than 30 seconds
+						{
 							endTime = System.currentTimeMillis();
 							diffTime = endTime - startTime;
 							score++;
-							System.out.println("Right!"); 
-							System.out.println("Score=" + score);
-							System.out.println("elapsed time: " + diffTime/millisToSeconds + " seconds of " + secondsLimit);
-					}
-				else
-					{
-					System	.out.println("time is up!");
+							System.out.println("Right! " + "Score= " + score + "; " + 
+							"seconds= " + diffTime/millisToSeconds + " out of " + secondsLimit + "\n"); 	
+						}
+					
+				
+					else // tell user when test running time is more than 30 seconds
+						{
+						endTime = System.currentTimeMillis();
+						diffTime = endTime - startTime;
+						System.out.println("time is up!");
+						System.out.println("Total test time you used was: " + secondsLimit);
+						break;
+						}
+				}
+			
+				else // when the user input is wrong
+				{
+					long endTime = System.currentTimeMillis();
+					long diffTime = endTime - startTime;
+					System.out.println("Wrong!" + " " + "Should be:" + SHORT_NAMES[randomIndex] + "\nYou cannot continue!");
+				
+					if (diffTime/millisToSeconds < secondsLimit)
+						{
+				
+							System.out.println("Total test time you used was: " + diffTime/millisToSeconds + " seconds");
+						}
+					
+					else
+						{
+							System.out.println("Total test time you used was: " + secondsLimit + " seconds");
+						}
+					
 					break;
-					}
-			}
-			else 
-			{
-				System.out.println("Wrong!" + " " + "Should be:" + SHORT_NAMES[randomIndex] + "\nYou cannot continue!");
-				scan.close();
-				long endTime = System.currentTimeMillis();
-				long diffTime = endTime - startTime;
-				System.out.println("tota test time is: " + diffTime/millisToSeconds);
-				break;
+				}
 			}
 		} while (b);
-		
+			
 		System.out.println("Test ends with score of " + score);
-		
+			
 	}		
 }
