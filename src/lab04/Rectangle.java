6c4d78d26928a873c0153df8473cc8b99525029d
Lei Zhao <Lei Zhao@lzhao13>
2016-09-20 20:46:43 -0400

    initial commit

--- src/lab04/Rectangle.java
+++ src/lab04/Rectangle.java
@@ -2,34 +2,31 @@ package lab04;
 
 public class Rectangle
 {
+	double width;
+	double height;
+	
+	public Rectangle(double width, double height)
+	{
+		this.width = width;       //cannot calculate without these two
+		this.height = height;  
+	}
 
-			private final double width;
-			private final double height;
-			
-			public Rectangle(double width, double height)
-			{
-				this.width = width;
-				this.height = height;
-			}
+	public double getArea()
+	{
+		return width * height;
+	}
+	
+	public double getPerimeter()
+	{
+		return 2 * width + 2 * height;
+	}
 
-			public double getArea() 
-			{
-				return width * height;
-			}
-			
-			public double getPerimeter()
-			{
-				return 2 * width + 2 * height;
-			}
-			
-			public static void main(String[] args) throws Exception
-			{
-				double a = 10d;
-				double b = 5d;
-				Rectangle r = new Rectangle (a,b);
-				System.out.println(r.getArea());
-				System.out.println(r.getPerimeter());
-			}
+	public static void main(String[] args) throws Exception
+	{
+		Rectangle r = new Rectangle(10,5);
+		System.out.println(r.getArea());
+		System.out.println(r.getPerimeter());
+	}
 }
 
 
