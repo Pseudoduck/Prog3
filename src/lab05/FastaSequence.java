3ee63881362b28a04a5e3fe00aa7c0a1e1a99565
Lei Zhao <Lei Zhao@lzhao13>
2016-10-02 11:16:59 -0400

    for grading

--- src/lab05/FastaSequence.java
+++ src/lab05/FastaSequence.java
@@ -1,66 +1,55 @@
 package lab05;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FastaSequence 
-{	
+{
 	private String header;
 	private String sequence;
-	
+
 	public FastaSequence(String header, String sequence)
 	{
 		this.header = header;
 		this.sequence = sequence;
 	}
-	
-	public static void main(String[] args) throws Exception
-	{
-		List<FastaSequence> fastaList = 
-				FastaSequence.readFastaFile("D:\\Programming\\Cygwin64\\home\\Lei Zhao\\sample.fasta");
 
-		for( FastaSequence fs : fastaList)
-		{
-			System.out.println(fs.getHeader());
-			System.out.println(fs.getSequence());
-			System.out.println(fs.getGCRatio());
-		}
-	}
-	
-	public static List<FastaSequence>readFastaFile(String filepath) throws Exception
+	public static List <FastaSequence> readFastaFile(String filepath) throws Exception
 	{
 		List<FastaSequence> fList = new ArrayList<FastaSequence>();
-		BufferedReader reader = new BufferedReader(new FileReader(new File(filepath)));
-		String line = reader.readLine();
+		BufferedReader reader = new BufferedReader(new FileReader(filepath));
+		String line="";
 		String fastaHeader ="";
 		String fastaSeq="";
 		
-		while (line != null) 
-			{
-			if (line.charAt(0) == '>')
-				fastaHeader = line.substring(1);
+		while ((line=reader.readLine()) != null)
+		{	
+			if (line.charAt(0)== '>')
+				{
+					fastaHeader = line.substring(1);
+				}
 			else
-				fastaSeq = line;
-				fList.add(new FastaSequence(fastaHeader,fastaSeq));
-			reader.close();
-			}
-		return fList;
-				
+				{
+					fastaSeq = line;
+					fList.add(new FastaSequence(fastaHeader,fastaSeq));
+				}
+		}
+		reader.close();
+		return fList;			
 	}	
 		
 	public String getHeader()
 	{
-		return header; 
+		return this.header; 
 	}
 		
 	public String getSequence()
 	{
-		return sequence; 
+		return this.sequence; 
 	}
-		
+	
 	public float getGCRatio()
 	{
 		float numC = 0;
@@ -76,5 +65,17 @@ public class FastaSequence
 		return ratio;
 	}
 		
+	public static void main(String[] args) throws Exception
+	{
+		List<FastaSequence> fastaList = 
+				FastaSequence.readFastaFile("D:\\Programming\\Cygwin64\\home\\Lei Zhao\\sample.fasta");
+		
+		for( FastaSequence fs : fastaList)
+		{
+			System.out.println(fs.getHeader());
+			System.out.println(fs.getSequence());
+			System.out.println(fs.getGCRatio());
+		}
+	}
 	
 }
